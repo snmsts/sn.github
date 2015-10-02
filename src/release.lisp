838a1d405205024938e1f3f863f2341f9ab1e84c
SANO Masatoshi <snmsts@gmail.com>
2015-10-03 08:15:32 +0900

    May be passing back-quote to jonathan is the problem.(guessing)

--- src/release.lisp
+++ src/release.lisp
@@ -4,7 +4,7 @@
 (in-package :sn.github.release)
 
 (defun releases-list (owner repo)
-  (jonathan:parse(cl-gists.util:get-request (quri:uri (format nil "~A/repos/~A/~A/releases"
+  (jonathan:parse (cl-gists.util:get-request (quri:uri (format nil "~A/repos/~A/~A/releases"
                                                cl-gists.api::+api-base-uri+   
                                                owner
                                                repo)))))
@@ -31,34 +31,34 @@
                                      tag)))))
 
 (defun release-create (owner repo tagname &key commitish (name "") (body "") draft prerelease)
-  (let ((uri (quri:uri (format nil "~A/repos/~A/~A/releases"
-                               cl-gists.api::+api-base-uri+
-                               owner
-                               repo)))
-        (content (jonathan:to-json `(("tag_name" . ,tagname)
-                                     ,@(when commitish `(("target_commitish" . ,commitish)))
-                                     ("name" . ,name)
-                                     ("body" . ,body)
-                                     ("draft" . ,(if draft t :false))
-                                     ("prerelease" . ,(if prerelease t :false)))
-                                   :from :alist))
-        (cl-gists:*credentials* t))
+  (let* ((uri (quri:uri (format nil "~A/repos/~A/~A/releases"
+                                cl-gists.api::+api-base-uri+
+                                owner
+                                repo)))
+         (to-json `(("tag_name" . ,tagname)
+                    ,@(when commitish `(("target_commitish" . ,commitish)))
+                    ("name" . ,name)
+                    ("body" . ,body)
+                    ("draft" . ,(if draft t :false))
+                    ("prerelease" . ,(if prerelease t :false))))
+         (content (jonathan:to-json to-json :from :alist))
+         (cl-gists:*credentials* t))
     (jonathan:parse (cl-gists.util:post-request uri :content content))))
 
 (defun release-edit (owner repo id tagname &key commitish (name "") (body "") draft prerelease)
-  (let ((uri (quri:uri (format nil "~A/repos/~A/~A/releases/~A"
-                               cl-gists.api::+api-base-uri+
-                               owner
-                               repo
-                               id)))
-        (content (jonathan:to-json `(("tag_name" . ,tagname)
-                                     ,@(when commitish `(("target_commitish" . ,commitish)))
-                                     ("name" . ,name)
-                                     ("body" . ,body)
-                                     ("draft" . ,(if draft t :false))
-                                     ("prerelease" . ,(if prerelease t :false)))
-                                   :from :alist))
-        (cl-gists:*credentials* t))
+  (let* ((uri (quri:uri (format nil "~A/repos/~A/~A/releases/~A"
+                                cl-gists.api::+api-base-uri+
+                                owner
+                                repo
+                                id)))
+         (to-json `(("tag_name" . ,tagname)
+                    ,@(when commitish `(("target_commitish" . ,commitish)))
+                    ("name" . ,name)
+                    ("body" . ,body)
+                    ("draft" . ,(if draft t :false))
+                    ("prerelease" . ,(if prerelease t :false))))
+         (content (jonathan:to-json to-json :from :alist))
+         (cl-gists:*credentials* t))
     (jonathan:parse (cl-gists.util:patch-request uri :content content))))
 
 (defun release-delete (owner repo id)
@@ -97,15 +97,15 @@
     (jonathan:parse (cl-gists.util:get-request uri))))
 
 (defun release-asset-edit (owner repo id name &key label)
-  (let ((uri (quri:uri (format nil "~A/repos/~A/~A/releases/assets/~A"
-                               cl-gists.api::+api-base-uri+
-                               owner
-                               repo
-                               id)))
-        (content (jonathan:to-json `(("name" . ,name)
-                                     ,@(when label `(("label" . ,label))))
-                                   :from :alist))
-        (cl-gists:*credentials* t))
+  (let* ((uri (quri:uri (format nil "~A/repos/~A/~A/releases/assets/~A"
+                                cl-gists.api::+api-base-uri+
+                                owner
+                                repo
+                                id)))
+         (to-json `(("name" . ,name)
+                    ,@(when label `(("label" . ,label)))))
+         (content (jonathan:to-json to-json :from :alist))
+         (cl-gists:*credentials* t))
     (jonathan:parse (cl-gists.util:patch-request uri :content content))))
 
 (defun release-asset-delete (owner repo id)
